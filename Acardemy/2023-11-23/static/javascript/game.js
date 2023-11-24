f40105bdbf0369f15a402643b60e6bce29401a42
nevnerver <nevnerver@naver.com>
2023-11-24 17:26:58 +0900

    23/11/24/17:26

--- Acardemy/2023-11-23/static/javascript/game.js
+++ Acardemy/2023-11-23/static/javascript/game.js
@@ -20,13 +20,21 @@ $(function () {
         $(this).css("color", "white");
         var idx = $(".numBox").index(this);
         board[idx] = 0;
+        alert(board);
         endgame();
     });
 
-
-
 });
 
-function endgame() {
+var row = 0; // 가로 빙고 
+var col = 0; // 세로 빙고
+var cross = 0; // 대각선 빙고
 
+function endgame() {
+    for (var k = 0; k < board.length; k++) {
+
+    }
 }
+
+$(".result").text("5줄 빙고로 게임이 끝났습니다");
+$(".result").text("6줄 이상 빙고로 게임 실패");
