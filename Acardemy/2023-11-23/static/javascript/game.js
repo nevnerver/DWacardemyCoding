0ca7683de95e5e8f28435b2822b781389dcd8593
nevnerver <nevnerver@naver.com>
2023-11-25 01:07:04 +0900

    23/11/25/01:06

--- Acardemy/2023-11-23/static/javascript/game.js
+++ Acardemy/2023-11-23/static/javascript/game.js
@@ -3,15 +3,13 @@ let board = new Array();
 //중복없이 25개 숫자 저장하기
 for (var i = 1; i <= 25; i++) {
     var tmp = Math.floor(Math.random() * 100) + 1;
-    if (board.indexOf(tmp) == -1)
-        board.push(tmp);
-    else
-        i--;
+    if (board.indexOf(tmp) == -1) board.push(tmp);
+    else i--;
 }
 
 $(function () {
-
-    $.each(board, function (i, v) { //i-인덱스, v-배열값
+    $.each(board, function (i, v) {
+        //i-인덱스, v-배열값
         $(".numBox").eq(i).text(v);
     });
 
@@ -23,18 +21,32 @@ $(function () {
         alert(board);
         endgame();
     });
-
 });
 
-var row = 0; // 가로 빙고 
+var row = 0; // 가로 빙고
 var col = 0; // 세로 빙고
 var cross = 0; // 대각선 빙고
 
-function endgame() {
-    for (var k = 0; k < board.length; k++) {
+var count = 0;
 
+let bingo = new Array();
+
+function endgame() {
+    for (var i = 0; i < board.length; i++) {
+        if (board[i] == 0 && bingo.indexOf(i) == -1) {
+            bingo.push(i);
+            bingo.sort(function (a, b) {
+                return a - b;
+            });
+        }
     }
+    // for (var k = 0; k < board.length; k++) {
+    //     if ( board[k]  )
+    // }
+
+    alert(bingo);
 }
 
 $(".result").text("5줄 빙고로 게임이 끝났습니다");
+
 $(".result").text("6줄 이상 빙고로 게임 실패");
